4d1897776e6b726a7bb8077e53851656aa8090f8
David Agudelo Ocampo <agudeloocampodavid@gmail.com>
2026-01-13 15:44:36 -0500

    better responsive image

--- src/app/(page)/Head/Slidepage.tsx
+++ src/app/(page)/Head/Slidepage.tsx
@@ -38,15 +38,16 @@ return (
     >
     {
     images.map( image=>(
-        <SwiperSlide key={image}>
+        <SwiperSlide key={image} className='relative w-full h-[500px] sm:h-[600px] md:h-[700px] lg:h-[800px]'>
 
             <Image
             src={image}
             alt={title}
             fill
             priority
-            sizes='100vw'
-            className=''>
+            quality={100} 
+            sizes='(max-width: 640px) 100vw, (max-width: 1024px) 100vw, 100vw'
+            className='object-cover'>
             </Image>
           
             <div className="absolute inset-0 bg-black/30" />
@@ -60,7 +61,6 @@ return (
   lg:top-[90%] lg:bottom-50 
   left-1/2
   transform -translate-x-1/2 -translate-y-1/2
-  
   w-[95%] sm:w-[85%] md:w-[80%] lg:w-[95%]
   gap-4 text-left z-20 px-10'>
         <h1 className='text-2xl sm:text-4xl md:text-4xl lg:text-4xl xl:text-4xl 
